e273e87ce88a9a308c55bffd902187ab8232db8f
YoannPull <pullyoann@gmail.com>
2026-01-14 10:07:43 +0100

    Remove non-legacy files from Git tracking

--- scripts/build_msa_cc.py
+++
@@ -1,4 +0,0 @@
-from carrefour.msa_pipeline.build_msa_cc import main
-
-if __name__ == "__main__":
-    main()
--- scripts/build_msa_lu.py
+++
@@ -1,4 +0,0 @@
-from carrefour.msa_pipeline.build_msa_lu import main
-
-if __name__ == "__main__":
-    main()
